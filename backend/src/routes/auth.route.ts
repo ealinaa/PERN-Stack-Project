e1ff6ff1b6cbed9997afbda3efee8b5b41419e98
ealinaa <alinaadhikari108@gmail.com>
2024-08-27 21:07:50 +0545

    worked with middleware

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,8 +1,11 @@
 import express from 'express'
-import { login, logout, signup } from '../controllers/auth.controller.js'
+import { login, logout, signup, getMe, } from '../controllers/auth.controller.js'
+import protectRoute from '../middleware/protectRoute.js'
+
 
 const router = express.Router()
 
+router.get("/me",protectRoute, getMe)//whenever u want to create message we would like to protect the route
 router.post("/login", login)
 router.post("/logout", logout)
 //http://localhost:5000 /api/auth/signup
