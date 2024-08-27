56d05989bb1ac287fb58179241c961bc3c8cbf2d
ealinaa <alinaadhikari108@gmail.com>
2024-08-27 18:45:10 +0545

    worked with check auth

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -3,6 +3,14 @@ import jwt from 'jsonwebtoken'
 import { Response } from 'express'
 
 const generateToken = (userId: string, res: Response) => {
+    const token = jwt.sign({ userId}, process.env.JWT_SECREt !, {
+        expiresIn: "15d"
+    })
+    res.cookie("jwt",token, {
+        maxAge: 15*24*60*60*1000
+
+    })
+
 
 }
 
