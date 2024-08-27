d4300b35df0eabe87159fab7017bd505dafe3bcc
ealinaa <alinaadhikari108@gmail.com>
2024-08-27 19:26:26 +0545

    worked with login and log out

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -57,5 +57,38 @@ export const signup = async(req:Request, res:Response) => {
     
 }
 
-export const login = async(req:Request, res:Response) => {}
-export const logout = async(req:Request, res:Response) => {}
+export const login = async(req:Request, res:Response) => {
+    try{
+        const { username, password} = req.body
+        const user = await prisma.user.findUnique({ where: { username } })
+        if(!user) {
+            return res.status(400).json({error: "Invalid cresentials"});
+        
+        }
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password)
+        if(!isPasswordCorrect) {
+            return res.status(400).json({error: "Invalid credentials"})
+        }
+        generateToken(user.id, res)
+        res.status(200).json({
+            id: user.id,
+            fullName: user.fullName,
+            username: user.username,
+            profilepic: user.profilePic
+        })
+
+    }catch (error: any) {
+        console.log("Error in ;ogin controller", error.message)
+        res.status(500).json({error: "Internal server Error"})
+
+    }
+}
+export const logout = async(req:Request, res:Response) => {
+    try{
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Log Out Successfully"})
+    }catch(error: any) {
+        console.log("Error in logout controller", error.message)
+        res.status(500).json({ error: "Internal Server Error"})
+    }
+}
